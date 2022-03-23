e3e334acef0619d7ccf0b8b37c0f1314170a7b1a
Susaid8 <97367629+Susaid8@users.noreply.github.com>
2022-03-23 17:13:01 -0500

    Problem 9 is solved

--- lab.js
+++ lab.js
@@ -134,10 +134,10 @@ let passingScore = 7;
 //console.log
 //increment
 
-while ( score < passingScore) {
-  console.log("Your score is not high enough")
-  score++
-}
+// while ( score < passingScore) {
+//   console.log("Your score is not high enough")
+//   score++
+// }
 
 ////////// INTERMEDIATE PROBLEMS //////////
 
@@ -148,7 +148,14 @@ while ( score < passingScore) {
 
 */
 
-//CODE HERE
+let changeMyMind = false
+
+if (changeMyMind === true){
+  changeMyMind = false
+}else{
+  changeMyMind = true
+}
+console.log(changeMyMind)
 
 ////////// PROBLEM 10 //////////
 // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
